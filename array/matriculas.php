aef593850b9ce9857378188d24474e042e94ec69
Felipe Poiatti <felipe.poiatti348@gmail.com>
2026-03-26 23:44:33 -0300

    Aprendendo sobre arrays

--- array/matriculas.php
+++ array/matriculas.php
@@ -19,6 +19,13 @@ $novosAlunos = [
 
 $alunosDesmpacotamento = [...$alunos2025,'Robert', ...$novosAlunos];
 $alunos2026 = array_merge($alunos2025, $novosAlunos);
+array_push($alunos2026, 'Alice', 'Bob', 'Charlie');
+$alunos2026[] = 'Luiz';
+array_unshift( $alunos2025, 'Alberto', 'Cadu');
+
+echo array_pop($alunos2026) . PHP_EOL;
+
+echo array_shift($alunos2026) . PHP_EOL;
 
 var_dump($alunos2026);
 var_dump($alunosDesmpacotamento);
